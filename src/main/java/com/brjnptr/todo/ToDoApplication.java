c612ab9f335980e71024089343614b7ac20b0c7c
brjnptr <burjanp189@gmail.com>
2023-02-20 20:46:22 +0100

    update btn

--- src/main/java/com/brjnptr/todo/ToDoApplication.java
+++ src/main/java/com/brjnptr/todo/ToDoApplication.java
@@ -22,7 +22,7 @@ public class ToDoApplication implements CommandLineRunner {
         TaskEntity entity = new TaskEntity(1L,"Sorting", "Ordering the existing repositories");
         repo.save(entity);
 
-        TaskEntity entity1 = new TaskEntity(2L,"Cleaning", "Take all the trash out of the room");
+        TaskEntity entity1 = new TaskEntity(2L,"Cleaning", "Take all the old cd-s and floppy disks out");
         repo.save(entity1);
 
         TaskEntity entity3 = new TaskEntity(3L,"Cooking", "Put the chicken into the pan");
